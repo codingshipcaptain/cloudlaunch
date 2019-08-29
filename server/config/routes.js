1ca0cd699d892e2f2376a404f0bb11a10e2153be
CodingShipCaptain <codingshipcaptain@mailbox.org>
2019-08-29 14:46:52 -0500

    Clear Exam 2

--- server/config/routes.js
+++
@@ -1,9 +0,0 @@
-const controller = require('./../controllers/controller');
-
-module.exports = (app) => {
-    app.get('/api/all', controller.index),
-    app.get('/api/one/:id', controller.show),
-    app.post('/api/new', controller.new),
-    app.put('/api/update/:id', controller.update),
-    app.delete('/api/remove/:id', controller.destroy)
-}
